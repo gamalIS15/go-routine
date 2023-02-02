778981087e18fee9f2243f11d1120dd444540493
Tomway <tom.way777@gmail.com>
2023-02-02 20:10:40 +0700

    Penambahan baru

--- channel_test.go
+++ channel_test.go
@@ -20,3 +20,42 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(7 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Eko Kurniawan"
+}
+
+func TestChannelAsParam(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+
+	data := <-channel
+	fmt.Println(data)
+
+	time.Sleep(7 * time.Second)
+}
+
+// Channel in / out
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Eko Kurniawan"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(7 * time.Second)
+}
